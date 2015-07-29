84e80f2eaf1ce19fc38aff09479e041c787d03ba
Kevin Gilbert <kevinmgilbert06@gmail.com>
2015-07-29 02:09:02 -0700

    implement file-parsing initialization step
    Read preference files from command line instead of hard-coding
    the dictionaries in memory at the beginning of the script file.

--- graph.py
+++ graph.py
@@ -1,17 +1,21 @@
-men = { "Xavier" : ["Alice", "Brenda", "Claire"], 
-		"Yuri" : ["Brenda", "Alice", "Claire"],
-		"Zoran" : ["Alice", "Brenda", "Claire"]}
-women = { "Alice" : ["Yuri", "Xavier", "Zoran"],
-		  "Brenda" : ["Xavier", "Yuri", "Zoran"],
-		  "Claire" : ["Xavier", "Yuri", "Zoran"]}
+def parse_prefs(pref_file):
+	prefs = {}
+	with open(pref_file, 'r') as pf:
+		lines = [line.rstrip() for line in pf]
+		header = [i for i, x in enumerate(lines) if "\t" not in x]
+		header.append(len(lines))
+		for f, l in zip(header[:-1], header[1:]):
+			nested = lines[(f + 1):l]
+			prefs[lines[f]] = [x.lstrip() for x in nested]
+	return prefs
 
 def ignore(sequence, value):
 	return [x for x in sequence if x != value]
 
 class Graph:
-	free = {}
-	men = {}       # who can he still propose to?
-	women = {}     # who would she prefer?
+	free = {}       # who is still eligible?
+	men = {}        # who can he still propose to?
+	women = {}      # who would she prefer?
 	marriages = {}  # who is she engaged to?
 	
 	def __init__(self, men, women):
@@ -71,8 +75,23 @@ class Graph:
 			else:
 				self.reject(juliet, romeo)
 
-graph = Graph(men, women)
-bipartite = graph.perfectMatching()
-for pair in bipartite:
-	print pair, "...", bipartite[pair]
+def init_parser():
+	from optparse import OptionParser
+	parser = OptionParser()
+	parser.add_option("-b", "--bachelors", dest = "Men", type = "string", help = "")
+	parser.add_option("-p", "--houses", dest = "Women", type = "string", help = "")
+	(options, args) = parser.parse_args()  # user input is stored in "options"
+	return options
+
+def main():
+	options = init_parser()
+	men = parse_prefs(options.Men)
+	women = parse_prefs(options.Women)
+	graph = Graph(men, women)
+	bipartite = graph.perfectMatching()
+	for pair in bipartite:
+		print pair, "...", bipartite[pair]
+
+if __name__ == "__main__":
+	main()
 
