ad2a659e20bddc5fb3520d507a1b6c1e8bef3f83
Kevin Gilbert <kevinmgilbert06@gmail.com>
2015-09-14 23:27:33 -0700

    create tree mapping associations

--- families/visualize.py
+++ families/visualize.py
@@ -1,4 +1,5 @@
 from optparse import OptionParser
+import sys
 
 def init_parser():
     parser = OptionParser()
@@ -8,6 +9,20 @@ def init_parser():
 
 def main():
     options = init_parser()
+    if options.File == None:
+        print "Error: Did not specify input file."
+        sys.exit(1)
+    
+    pairs = []
+    with open(options.File, 'r') as input_file:
+        for line in input_file:
+            pairs.append(line.strip())
+    mapping = {}
+    for p in pairs:
+        (parent, children) = p.split(":")
+        if parent not in mapping:
+            mapping[parent] = []
+        mapping[parent].extend(children.split(","))
 
 if __name__ == "__main__":
     main()
